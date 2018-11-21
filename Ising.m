b30814b40acfd5f53cbd0caaf1ec0cb087b0e65b
Jared Dobry <jareddobry10@gmail.com>
2018-11-21 15:54:05 -0500

    Update Ising.m

--- Ising.m
+++ Ising.m
@@ -17,9 +17,14 @@ for i = 1:length(NMatrix)
         end
     end
     %Calculate magnetization and dispersion
-    m = sum(sum(a))/N^2;
-    m2 = sum(sum(a.^2))/N^2; %This will always just be 1/N so this is not the way to do this
-    dispersion = m2 - m^2; %Note: this dispersion is not correct I think, I think we have to do multiple trials to find the average value of m^2
-    s = sprintf('%d x %d matrix generated random magnetization %f and dispersion %f', N, N, m, dispersion);
+    m(i) = sum(sum(a))/N^2;
+    m2(i) = m(i)^2; %This will always just be 1/N so this is not the way to do this
+    s = sprintf('Grid of %d x %d had magnetization %f', N, N, m(i));
     disp(s);
 end
+%Calculate dispersion
+avgM2 = sum(m2)/length(m2);
+avgM = sum(m)/length(m);
+dispersion = avgM2 - avgM^2; %Note: this dispersion is not correct I think, I think we have to do multiple trials to find the average value of m^2
+s = sprintf('dispersion %f', dispersion);
+disp(s);
