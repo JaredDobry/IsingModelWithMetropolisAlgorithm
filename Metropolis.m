7b77c71338daa7e906b4a3a9dd5d76cca0e80d3f
Jamie Mazza <jamiebmazza@gmail.com>
2018-12-07 15:13:53 -0500

    Added running averages for energy and magnetization

--- Metropolis.m
+++ Metropolis.m
@@ -2,10 +2,29 @@ function [resultingMatrix] = Metropolis(width, height, iterations, J, Beta)
 %METROPOLIS performs metropolis algorithm starting with matrix of all 1
 matrix = -ones(height, width);
 
+magnetization = zeros(1, iterations + 1);
+energy = zeros(1, iterations + 1);
+
 for i=1:iterations
    matrix=MetropolisStep(matrix, J, Beta);
+   magnetization(i + 1) = (sum(sum(matrix))/(width*height))/i + magnetization(i);
+   energy(i + 1) = -J*sum(sum(matrix))/i + energy(i);  
 end
+
+%popping off the first element of the arrays
+magnetization(1) = [];
+energy(1) = [];
+
+plot(magnetization,'o');
+title('Mean Magnetization');
+xlabel('iterations');
+ylabel('magnetization');
+figure();
+plot(energy,'o');
+title('Mean Energy');
+xlabel('iterations');
+ylabel('energy');
+
 
 resultingMatrix = matrix;
 end
-
